1d1f7573a9bb9a9e6f014f3c3816d81513effe49
anz <g_anz@mail.ru>
2024-03-02 23:23:45 +0300

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+# basic_qrcode.py
+import csv
+import segno
+
+# qrcode = segno.make_qr("Привет")
+# qrcode.save("basic_qrcode.png")
+
+with open('test.csv', 'r', newline='', encoding="cp1251", errors='ignore') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    for row in spamreader:
+        qrcode = segno.make_qr(row[1])
+        qrcode.save(f"{row[0]}.png", scale=5)
+        print(row)
